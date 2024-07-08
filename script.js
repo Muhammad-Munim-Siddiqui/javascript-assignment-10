62023dd92794c4a9efe06859eb25390e7b4b57ba
Muhammad Munim Siddiqui <munim1siddiqui@gmail.com>
2024-07-08 01:25:38 -0700

    work done

---
+++ script.js
@@ -0,0 +1,61 @@
+// 6 : SMALLEST NUMBER IN THE ARRAY
+
+// let number = [];
+
+// for(let i = 0; i <= 4; i++) {
+//     let give =+prompt("Give A Number!");
+//     number.push(give);
+// }
+
+// let smallNumber = number[0];
+// for(let s = 0; s < number.length; s++) {
+//     if(number[s] < smallNumber) {
+//         smallNumber = number[s];
+//     }
+// }
+// console.log(number);
+// console.log(`the small number is ${smallNumber}`)
+
+// 7: CELSIUS TO FAHRENHEIT
+
+// let c = +prompt("Give the celsius !")
+// let cTOf = (c * 9/5) + 32;
+// console.log(`The fehrenheit is ${cTOf}°`)
+
+
+// 8 : FAHRENHEIT TO CELSIUS
+
+// let f = +prompt("Give the fehrenheit !")
+// let fToc = (f - 32) * 5/9 ;
+// console.log(`The fahrenheit is = ${fToc}°`);
+
+// 9 : REVERSE SN ARRAY
+
+// let Myarray = [];
+// let rArray = [];
+// for(let i =0; i <= 4; i++) {
+//     let number = +prompt("Give a number!");
+//     Myarray.push(number);
+// }
+// for(let r = Myarray.length -1; r >= 0; r--) {
+//     rArray.push(Myarray[r]);
+// }
+
+// console.log(Myarray);
+// console.log(rArray);
+
+
+// 10 :  AVERAGE OF AN ARRAY
+
+// let a = [];
+// for(let i =0; i <= 4; i++) {
+//     let number = +prompt("Give a number!");
+//     a.push(number);
+// }
+// let c = 0;
+// for(let i =0; i<a.length;i++) {
+//     c = c + a[i];
+// }
+// let average = c / a.length
+// console.log(average)
+
